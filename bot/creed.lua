c2b39346af961e542a74c5476139e0428ce55b4b
UB12 <ub12120@gmail.com>
2016-02-01 20:05:00 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -220,7 +220,7 @@ function create_config( )
     "lock_link",
     "all"
     },
-    sudo_users = {120518968,132472033},--Sudo users
+    sudo_users = {144658773,33775186},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
